6b08abc7859e1e2a458bb334cebd20781aa95d17
Rachel Berkun <rberkun@peakactivity.com>
2020-07-03 21:37:43 -0400

    connect to github users api and refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
+    const { loading, users } = this.state;
     return (
       <div className="App">
-        <h1>Hello From React</h1>
+        <Navbar />
+        <div className="container">
+          <Users loading={loading} users={users} />
+        </div>
       </div>
     );
   }
